77c5cd66d0cef4df47a2f1327315843b11809ab3
Aly Raffauf <aly@aly.codes>
2026-05-23 12:29:35 -0400

    flake: bump to nixos-unstable and home-manager/master

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Aly's NixOS homelab flake with flake-parts";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     actions-nix = {
       url = "github:alyraffauf/actions.nix";
@@ -27,7 +27,7 @@
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
