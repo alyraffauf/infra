1e3e5188c18b3006f9ab162915cb0234f030d968
Aly Raffauf <aly@aly.codes>
2026-05-25 21:50:07 -0400

    flake: pin to nixos-26.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Aly's NixOS homelab flake with flake-parts";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
 
     actions-nix = {
       url = "github:alyraffauf/actions.nix";
