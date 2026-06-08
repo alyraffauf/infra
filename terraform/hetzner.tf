318f35ff2d3dbf0406a9c3f17d6499955b2c0377
Aly Raffauf <aly@aly.codes>
2026-06-08 18:54:09 -0400

    terraform/hetzner: import firewall

--- terraform/hetzner.tf
+++ terraform/hetzner.tf
@@ -1,8 +1,74 @@
+resource "hcloud_firewall" "solaceon" {
+  name = "solaceon"
+
+  rule {
+    direction  = "in"
+    protocol   = "tcp"
+    port       = "22"
+    source_ips = ["0.0.0.0/0", "::/0"]
+  }
+
+  rule {
+    direction  = "in"
+    protocol   = "tcp"
+    port       = "80"
+    source_ips = ["0.0.0.0/0", "::/0"]
+  }
+
+  rule {
+    direction  = "in"
+    protocol   = "tcp"
+    port       = "443"
+    source_ips = ["0.0.0.0/0", "::/0"]
+  }
+
+  rule {
+    direction  = "in"
+    protocol   = "tcp"
+    port       = "2222"
+    source_ips = ["0.0.0.0/0", "::/0"]
+  }
+
+  rule {
+    direction  = "in"
+    protocol   = "tcp"
+    port       = "23"
+    source_ips = ["0.0.0.0/0", "::/0"]
+  }
+
+  rule {
+    direction  = "in"
+    protocol   = "tcp"
+    port       = "8282"
+    source_ips = ["0.0.0.0/0", "::/0"]
+  }
+
+  rule {
+    direction  = "in"
+    protocol   = "tcp"
+    port       = "8383"
+    source_ips = ["0.0.0.0/0", "::/0"]
+  }
+
+  rule {
+    direction  = "in"
+    protocol   = "udp"
+    port       = "41641"
+    source_ips = ["0.0.0.0/0", "::/0"]
+  }
+
+  rule {
+    direction  = "in"
+    protocol   = "icmp"
+    source_ips = ["0.0.0.0/0", "::/0"]
+  }
+}
+
 resource "hcloud_server" "solaceon" {
   name         = "solaceon"
   server_type  = "cx22"
   image        = "ubuntu-24.04"
   location     = "nbg1"
   backups      = true
-  firewall_ids = [2127864]
+  firewall_ids = [hcloud_firewall.solaceon.id]
 }
