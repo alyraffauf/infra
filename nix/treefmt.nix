5df48da353d470af8c5e7a640dcad36b96f37511
Aly Raffauf <aly@aly.codes>
2026-07-01 18:16:54 -0400

    treefmt: exclude k8s/flux/system/gotk-components.yaml

--- nix/treefmt.nix
+++ nix/treefmt.nix
@@ -1,6 +1,11 @@
 {
   perSystem.treefmt.config = {
-    settings.global.excludes = ["k8s/flux/secrets/*.sops.yaml"];
+    settings.global.excludes = [
+      "k8s/flux/secrets/*.sops.yaml"
+      # Flux-generated upstream manifest (flux install output); Renovate bumps
+      # it verbatim, so don't let prettier reformat/diverge it from upstream.
+      "k8s/flux/system/gotk-components.yaml"
+    ];
 
     programs = {
       alejandra.enable = true;
