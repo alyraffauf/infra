f310d4d3fdb083956aaed9b90de0ccef2a8f9b02
Aly Raffauf <aly@aly.codes>
2026-08-24 10:25:45 -0400

    chore: enable Nix linters

--- nix/treefmt.nix
+++ nix/treefmt.nix
@@ -3,9 +3,11 @@ _: {
     treefmt.config = {
       programs = {
         alejandra.enable = true;
+        deadnix.enable = true;
         prettier.enable = true;
         shellcheck.enable = true;
         shfmt.enable = true;
+        statix.enable = true;
         terraform.enable = true;
       };
 
